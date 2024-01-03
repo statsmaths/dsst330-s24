8518415b0076bf14c07fbc1def62bc195d0f1d2d
statsmaths <taylor@dvlab.io>
2024-01-03 14:49:56 -0500

    auto update

--- funs/update.R
+++ funs/update.R
@@ -2,7 +2,7 @@
 #' currently exist locally.
 .update_materials <- function()
 {
-  base <- "https://raw.githubusercontent.com/statsmaths/dsst289-s24/main"
+  base <- "https://raw.githubusercontent.com/statsmaths/dsst330-s24/main"
 
   # what files are available to download?
   today <- as.character(Sys.Date())
